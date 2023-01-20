6c9e42595fd6a3798771c4dd2c7d0c1b69a074d6
Alukon <111476972+Alukon@users.noreply.github.com>
2023-01-20 17:24:53 +1000

    Исполнение с английского на русский

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import gtts
 from googletrans import Translator
 from tika import parser
 
-pdf = parser.from_file('b7.pdf')
+pdf = parser.from_file('b77.pdf')
 print(pdf['content'])
-
-translator = Translator()
-translated = translator.translate(pdf['content'], dest='ru')
-translated_2 = translated.text
-
-tts = gtts.gTTS(translated_2, lang='ru')
-tts.save('audiobook3.mp3')
-
+# #Перевод сразу на русский-----------------------------------------
+# translator = Translator()
+# translated = translator.translate(pdf['content'], dest='ru')
+# translated_2 = translated.text
+# print(translated_2)
+#
+# tts = gtts.gTTS(translated_2, lang='ru')
+# tts.save('audiobook3.mp3')
+# -------------------------------------------------------
+tts = gtts.gTTS(pdf['content'], lang='en')
+tts.save('audiobook2.mp3')
